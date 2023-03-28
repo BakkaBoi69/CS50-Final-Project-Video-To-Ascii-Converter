39b1c1a64c4bd2cd4dc66b601d01246fc3a4aade
BakkaBoi69 <98868015+BakkaBoi69@users.noreply.github.com>
2023-03-28 22:48:36 +0530

    ffmpeg should work

--- module_installer.py
+++ module_installer.py
@@ -1,2 +1,8 @@
 import subprocess
 subprocess.run("pip install -r requirements.txt", shell=True)
+
+# Set the command to append the export statement to the .zshrc file
+command = 'echo "export IMAGEIO_FFMPEG_EXE=./ffmpeg" >> ~/.zshrc'
+
+# Run the command using subprocess
+subprocess.run(command, shell=True)
